d80898b4cea9a70982577d4c53b26394b7af7fb0
Carlos Eduardo <carlossallet2012@gmail.com>
2024-05-21 15:41:19 -0400

    alt

--- server.js
+++ server.js
@@ -13,7 +13,7 @@ const wss = new WebSocket.Server({ server });
 wss.on('connection', (ws) => {
     console.log('Cliente conectado');
 
-    ws.send('Bem-vindo ao servidor WebSocket!');
+    ws.send('Bem-vindo ao quiz maluco!');
 
     ws.on('message', (message) => {
         console.log(`Mensagem recebida: ${message}`);
